949ecd1119abce88e001705ab07db4cb1a3ec41a
tony_wu <wuzhongru@gmail.com>
2018-09-24 09:08:20 -0700

    add one code

--- src/interview/dropbox/CycleArray.java
+++ src/interview/dropbox/CycleArray.java
@@ -1,4 +1,36 @@
 package interview.dropbox;
 
 public class CycleArray {
+
+    int[] array;
+    int index;
+    int bound;
+    int start;
+
+    CycleArray(int size){
+        bound = size;
+        index = 0;
+        start = 0;
+
+        array = new int[bound];
+    }
+
+    int poll(){
+        if(start == index){
+            return -1;
+        }
+
+        if()
+    }
+
+    void offer(int value){
+
+    }
+
+    boolean hasNext(){
+
+    }
+
+
+
 }
